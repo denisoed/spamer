9352b014f1fd2d6e0992906e75856b4cf920430d
Gulbustan <gbaudunova@gmail.com>
2017-11-20 10:11:18 +0600

    Added tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+import sys
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+import requests
+from .views import *
 
-# Create your tests here.
+client = Client()
+
+
+class ConnectionTest(TestCase):
+    def test_for_connection(self):
+        responce = self.client.get('http://127.0.0.1:8000/main/')
+        self.assertEqual(responce.status_code, 200)
+
+
+class mainTest(TestCase):
+    def testCatch_data(self):
+        responce = self.client.get('/main/', {'title': 'title', 'url': 'http://grablib.org', 'description': 'descr'})
+        self.assertEqual(responce.status_code, 200)
+
+
+
+
+        # request = self.client.post(reverse('catch_data'), data)
+        # request.client = self.client
+        #
+        # responce = MainFormsCreateView.as_view()(request)
+        # self.assertEqual(responce.status_code, 302)
+
+
+
+
+
+
+
+
+
+
