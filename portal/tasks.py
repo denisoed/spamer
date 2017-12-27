26d449f6dbb6b927df1b915538cb525fcbc6ca0d
Gulbustan <gbaudunova@gmail.com>
2017-12-27 19:00:53 +0600

    remove unusable handlers

--- portal/tasks.py
+++ portal/tasks.py
@@ -2,7 +2,7 @@
 import logging
 
 
-from grab import Grab, DataNotFound
+from grab import Grab
 from grab.util.log import default_logging
 from portal.list_portals import list_portals
 from celery.task import task
@@ -15,42 +15,6 @@ LOGGER.setLevel(logging.DEBUG)
 
 # Init grab
 GRAB = Grab()
-
-
-# //////////////// Auth portal ///////////////// #
-def get_login_page(portal, login, password):
-    """ Authenticate selected portal """
-    url_login = portal['url_auth']
-    GRAB.setup(timeout=10, connect_timeout=10)
-    page_auth = GRAB.go(
-        url_login, log_file='templates/grab/bug_auth_portal.html')
-    page_auth.text_search(portal['auth_by'])
-    return auth_portal(page_auth, portal, login, password)
-
-
-def auth_portal(page_auth, portal, login, password):
-    """ Authenticate selected portal """
-    try:
-        page_auth.set_input(portal['inp_login'], login)
-        page_auth.set_input(portal['inp_password'], password)
-        sub_resp = page_auth.submit()
-        auth_form = sub_resp.text_search(portal['auth_complete'])
-        if auth_form is True:
-            return True
-        else:
-            return "Введите корректный логин или пароль"
-    except DataNotFound:
-        return "Вы уже аутентифицированы"
-
-
-def go_authenticate(request, portal, login, password):
-    login_page = get_login_page(portal, login, password)
-    if login_page is True:
-        # messages.success(request, "Аутентификация прошла успешно!")
-        return True
-    else:
-        # messages.error(request, login_page)
-        return False
 
 
 # //////////////// Send spam ///////////////// #
@@ -76,22 +40,13 @@ def send_spam(input_data, portals):
             portals_list[p]['inp_url'], input_data['url'])
         GRAB.doc.set_input(
             portals_list[p]['inp_text'], input_data['description'])
-        GRAB.doc.submit()
+        # send(GRAB)
+    return True
 
 
 def send(portal):
     response = portal.doc.submit()
     return response
-
-
-def fill_fields(page, portal_input, input_data):
-    page.set_input(
-        portal_input['inp_title'], input_data['title'])
-    page.set_input(
-        portal_input['inp_url'], input_data['url'])
-    page.set_input(
-        portal_input['inp_text'], input_data['description'])
-    return page
 
 
 def get_selected_portal(port_list):
