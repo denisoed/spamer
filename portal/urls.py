48b6aaddcd80182185b0089588c17f63d7e6e289
Gulbustan <gbaudunova@gmail.com>
2017-12-27 19:01:14 +0600

    remove unusable urls

--- portal/urls.py
+++ portal/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import url
 from portal.views import create_portal, delete_portal
-from .tasks import get_login_page
 
 
 urlpatterns = [
     url(r'create/$', create_portal, name='create_portal'),
-    url(r'delete/(?P<id_portal>\d+)/$', delete_portal, name='delete_portal'),
-    url(r'get_login_page/(?P<portal>\w+)/(?P<login>\w+)/(?P<password>\w+)$',
-        get_login_page, name='get_login_page')
+    url(r'delete/(?P<id_portal>\d+)/$', delete_portal, name='delete_portal')
 ]
